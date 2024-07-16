78a34c8c8bdb54fe75cb864970f73bdf723c287c
qwes5674 <qwes5674@gmail.com>
2024-07-16 16:46:28 +0900

    jpql

--- chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/section02/parameter/Menu.java
+++ chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/section02/parameter/Menu.java
@@ -1,2 +1,31 @@
-package com.ohgiraffers.section02.parameter;public class Menu {
+package com.ohgiraffers.section02.parameter;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name ="menu_section02")
+@Table(name = "tbl_menu")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Menu {
+
+    @Id
+    @Column(name = "menu_code")
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "orderable_status")
+    private String orderableStatus;
 }
